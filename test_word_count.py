bf2ae75e73cd47655e09df8c5542fa6185a35319
Zzy <35677923+gwyn31@users.noreply.github.com>
2020-02-14 09:50:38 +0800

    Update test_word_count.py

--- test_word_count.py
+++ test_word_count.py
@@ -4,18 +4,20 @@ from pyspark.sql import SparkSession
 
 
 def main():
+    # 初始化一个Spark进程
     spark = SparkSession.builder.appName("test-pyspark-wordCount").master("local[4]").getOrCreate()
+    # 将文本文件读入RDD
     rdd = spark.sparkContext.textFile("./test_text.txt")
     word_count = rdd.filter(
-        lambda line: True if line != "" else False
+        lambda line: True if line != "" else False  # 去掉空白行
     ).flatMap(
-        lambda line: line.strip().split(" ")
+        lambda line: line.strip().split(" ")  # 每行用空格分割后展平
     ).map(
-        lambda word: (word.lower(), 1)
+        lambda word: (word.lower(), 1)  # 将每个词映射为(词 -> 1次)
     ).reduceByKey(
-        lambda v1, v2: v1 + v2
+        lambda v1, v2: v1 + v2  # reducer: 对相同的词进行词计数
     ).sortBy(
-        lambda kv: kv[1], ascending=False
+        lambda kv: kv[1], ascending=False  # 结果按值(频数)降序排列
     )
     for kv in word_count.collect():
         print(kv)
